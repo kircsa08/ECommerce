1c70e7c967671436d65d9b4632f43cc9b0274965
kircsa08 <kircsa08@gmail.com>
2023-08-29 21:18:19 +0200

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import Catalog from "../../features/catalog/Catalog";
 import { CssBaseline, Container, createTheme, ThemeProvider } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +26,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleChange={handleChange}/>
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,31 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { NavLink } from "react-router-dom";
+import { ShoppingCart } from "@mui/icons-material";
+
+const midLinks = [
+    {title: 'catalog', path:'/catalog'},
+    {title: 'about', path:'/about'},
+    {title: 'contact', path:'/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path:'/login'},
+    {title: 'register', path:'/register'}
+]
+
+const navStyles = {
+    color: 'inherit', 
+    textDecoration: 'none',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active':{
+        color: 'text.secondary'
+    }
+}
 
 interface Props{
     darkMode: boolean
@@ -10,22 +35,61 @@ interface Props{
 export default function Header({darkMode, handleChange}: Props){
     return (
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    ReStore
-                </Typography> 
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <Box
                     sx={{
                         display: 'flex',
                         alignItems: 'center',
-                        color: 'text.primary',
                         borderRadius: 1,
                         p: 3,
                     }}
                     >
+                <Typography variant="h6"
+                    component={NavLink}
+                    to="/"
+                    sx={navStyles}>
+                    ReStore
+                </Typography> 
                     <IconButton sx={{ ml: 1 }} onClick={handleChange} color="inherit">
                         {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
                     </IconButton>
+                </Box>
+                <List sx={{display: 'flex'}}>
+                    {
+                        midLinks.map(({title, path}) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))
+                    }
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size="large" edge="start" color='inherit' sx={{mr: 2}}>
+                        <Badge badgeContent='4' color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{display: 'flex'}}>
+                        {
+                            rightLinks.map(({title, path}) => (
+                                <ListItem
+                                    component={NavLink}
+                                    to={path}
+                                    key={path}
+                                    sx={navStyles}
+                                >
+                                    {title.toUpperCase()}
+                                </ListItem>
+                            ))
+                        }
+                    </List>
                 </Box>
             </Toolbar>
         </AppBar>
